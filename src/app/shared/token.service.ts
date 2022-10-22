f77e97a1692dbc11d06c17040244f03c09497189
jixenc <jixenc@miumg.edu.gt>
2022-10-22 15:47:29 -0600

    first commit

---
+++ src/app/shared/token.service.ts
@@ -0,0 +1,31 @@
+import { Token } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUserNamer';
+const AUTHORITIES_KEY = 'AuthAuthorities'
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class TokenService {
+    roles: Array<string> = [];
+
+    constructor (){}
+
+    public setToken(token: string):void {
+        window.sessionStorage.removeItem(TOKEN_KEY);
+        window.sessionStorage.setItem(TOKEN_KEY,token);
+    }
+    public getToken(): string {
+        return sessionStorage.getItem(TOKEN_KEY);
+    }
+
+    public setUserName(userName: string):void {
+        window.sessionStorage.removeItem(USERNAME_KEY);
+        window.sessionStorage.setItem(USERNAME_KEY,userName);
+    }
+    public getUserName(): string {
+        return sessionStorage.getItem(USERNAME_KEY);
+    }
+  }
